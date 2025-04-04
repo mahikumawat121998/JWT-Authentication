debda64a324cb85a1debe7ea8279967c6dcd8976
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-04 11:19:57 +0530

    Nre code

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const { connection } = require("./connect.js");
 const userRouter = require("./route/userRouter.js");
-const swaggerDocs = require("./swagger");
 const app = express();
 app.use(express.json());
-swaggerDocs(app);
 app.use("/user", userRouter);
 app.get("",(req,res)=>{
 res.status(201).json({name:"Mahesh"})
